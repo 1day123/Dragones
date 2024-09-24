697c7fc3947122e1ba8972328aa73a766491af83
1day123 <corazaroldandayra@gmail.com>
2024-09-24 09:53:03 -0600

    Configuraciones de producción y forzar HTTPS

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
         laravel({
             input: 'resources/js/app.jsx',
             refresh: true,
+            base: '/',
+
         }),
         react(),
     ],
